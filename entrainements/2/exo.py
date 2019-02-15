d847f4a428605dea70c76b81bcbef6395e2f34ea
Cédric JUNG <cedced19@gmail.com>
2019-02-15 09:47:28 +0100

    Check before

--- entrainements/2/exo.py
+++ entrainements/2/exo.py
@@ -1,5 +1,11 @@
 from fractions import Fraction
 
+def check (r): # Vérification 
+    s = 0
+    for i in range(len(r)):
+        s+=Fraction(1,r[i])
+    return(s==1)
+
 def rec (A, N):
     # On constate que pour trouver le x_p suivant il faut majoré par (N-len(A))/(1-1/x_0-1/x_1-...-1/x_p-1)
     # où len(A) est le nombre d'éléments trouvés
@@ -8,44 +14,43 @@ def rec (A, N):
     m=1
     for val in A:
         m=m-1/val
-    if (round(m,15)==0): # Si le flottant est trop proche de zero on le supprime, il doit y avoir un moyen de mieux faire, et de réduire les cas
+    if (round(m,10)<=0): # Si le flottant est trop proche de zero on le supprime, il doit y avoir un moyen de mieux faire, et de réduire les cas
         return L
     M=round((N-len(A))/m)
     c=M
-    while (c<=M and c>=A[-1]):
-        L.append([*A, c])
-        c-=1
+    if (len(A)!=N-1):
+        while (c<=M and c>=A[-1]):
+            L.append([*A, c])
+            c-=1
+    else: # Si il n'y a plus qu'un nombre à trouver alors cela doit forcément être solution
+        while (c<=M and c>=A[-1]):
+            if (check([*A, c])):
+                #print([*A, c])
+                L.append([*A, c])
+                return L
+            c-=1
     return L
-
-def rec2 (x, N): # Construction des listes de proche en proche
-    L=[[x]]
-    count=1
-    while (count<N):
-        Lt=[]
-        for k in range (len(L)):
-            for val in rec(L[k], N):
-                Lt.append(val)
-        L=Lt
-        count+=1
-    return L
-
-
-def check (r): # Vérification 
-    s = 0
-    for i in range(len(r)):
-        s+=Fraction(1,r[i])
-    return(s==1)
 
 def solve (N):
     L=[[N]*N] # On ajoute directement la solution évidente
+    x = []
     for i in range(2, N):
-        r = rec2(i, N)
-        for i in r:
-            if(check(i)): # Vérification
-                L.append(i)
+        x.append([i])
+    count=1
+    while (count<N): # On construit la suite de proche en proche
+        Lt=[]
+        for k in range (len(x)):
+            for val in rec(x[k], N):
+                if (len(val) != 0):
+                    Lt.append(val)
+        x=Lt
+        count+=1
+    for i in x:
+        L.append(i)
     return L, len(L)
 
 # Tests
 print(solve(3))
 print(solve(4))
 print(solve(5))
+#print(solve(6))
